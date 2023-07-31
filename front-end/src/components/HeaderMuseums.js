fcc3fa8585457a44632f649d9891cdb0bc8c3603
MrGordon112 <claudiugordon458@gmail.com>
2023-07-31 17:47:10 +0300

    worked on bg

--- front-end/src/components/HeaderMuseums.js
+++ front-end/src/components/HeaderMuseums.js
@@ -16,7 +16,7 @@ function HeaderMuseums() {
 
   return (
     <div className='hero-container3'>
-      <video className="videoHero"  autoplay muted loop playsinline  src='https://firebasestorage.googleapis.com/v0/b/museum-ee649.appspot.com/o/videos%2Fyt5s.io-Van%20Gogh%20Museum%204K%20Virtual%20Tour%20__%20Compilation-(1080p).mp4?alt=media&token=c662a065-894a-4ed7-8395-ba8b5315bd4a' />
+      <video className="videoHero" src='https://firebasestorage.googleapis.com/v0/b/museum-ee649.appspot.com/o/videos%2Fyt5s.io-Van%20Gogh%20Museum%204K%20Virtual%20Tour%20__%20Compilation-(1080p).mp4?alt=media&token=c662a065-894a-4ed7-8395-ba8b5315bd4a' autoPlay loop muted />
       <h1>Our Museums</h1>
        <p className="website-description"> &nbsp;&nbsp; &nbsp; Here is a list of museums that
        already started to use our platform for their exponates, get lost in this online library.</p>
